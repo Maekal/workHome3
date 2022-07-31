229db73a29d7832da8d596ed16754198d106dce5
Михаил <maekal0692@yandex.ru>
2022-07-31 23:21:03 +0300

    Сделанно ДЗ 3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        //заданае 1
+        //Заданае 1
         int clientOS = 0;
         if (clientOS == 0) {
             System.out.println("Установите версию приложения для Android по ссылке");
